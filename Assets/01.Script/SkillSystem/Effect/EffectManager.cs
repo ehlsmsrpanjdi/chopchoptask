fed11d5d71b62fb37142acb2944e3be329507c55
ehlsmsrpanjdi <usm9401@gmail.com>
2025-10-01 11:36:48 +0900

    스킬 구조 변경할 필요성을 느끼고 변경 전 커밋

---
+++ Assets/01.Script/SkillSystem/Effect/EffectManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+public class EffectManager
+{
+    static EffectManager instance;
+
+    public static EffectManager Instance
+    {
+        get
+        {
+            if (null == instance)
+            {
+                instance = new EffectManager();
+                instance.Init();
+            }
+            return instance;
+        }
+    }
+
+    Dictionary<int, EffectBase> effectDictionary= new Dictionary<int, EffectBase>();
+
+    void Init()
+    {
+        effectDictionary.Add(1, new AttackEffect());
+    }
+}
